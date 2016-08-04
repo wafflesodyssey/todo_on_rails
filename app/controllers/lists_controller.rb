a7f731f705c05b6bf5dcabe7d439f628b2302a9f
tony un <tony un>
2016-08-04 09:16:47 -0400

    first commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,23 @@
+class ListsController < ApplicationController
+    def index
+        @lists = List.all
+    end
+
+    def new
+        @list = List.new
+    end
+
+    def create
+        @list = List.new(list_params)
+        @list.save
+        redirect_to list_path(@list)
+    end
+
+    def show
+      @list = List.find(params[:id])
+    end
+
+    private def list_params
+        params.require('list').permit(:name, :user_id)
+    end
+end
